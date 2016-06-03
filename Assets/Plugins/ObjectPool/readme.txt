8492665109c40b855c7398956245e2857f1eebcd
waking_man <zs9024@sina.com>
2016-06-03 10:49:40 +0800

    删除旧文件

--- Assets/Plugins/ObjectPool/readme.txt
+++
@@ -1,2 +0,0 @@
-说明：对象缓存池功能
-来源：http://unitypatterns.com/resource/objectpool/
--- Assets/Scriptes/Common/Event/EventListener.cs
+++
@@ -1,7 +0,0 @@
-﻿
-using System;
-using System.Runtime.CompilerServices;
-
-public delegate void EventListener(params object[] args);
-
-
